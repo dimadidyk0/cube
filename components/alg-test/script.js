d285950de62e52bc35e8df660fce7ba1aa3b9272
Dima <dimadidyk0@gmail.com>
2017-01-20 17:56:07 +0200

    one more block

---
+++ components/alg-test/script.js
@@ -0,0 +1,12 @@
+//
+
+
+
+
+var arr = document.getElementsByTagName("div");
+var button = document.getElementsByTagName("button");
+console.log(arr);
+
+
+
+button.addEventListener("click", displayDate);
