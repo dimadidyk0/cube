87e69bf7c3f2769c685713b2ab06a06b80c6bf0e
Dima <dimadidyk0@gmail.com>
2017-01-20 18:57:27 +0200

    array test

--- components/alg-test/script.js
+++ components/alg-test/script.js
@@ -3,10 +3,29 @@
 
 
 
-var arr = document.getElementsByTagName("div");
-var button = document.getElementsByTagName("button");
-console.log(arr);
+var array = document.getElementsByTagName("div");
+var button = document.getElementById("button");
 
+var newArr = [];
 
+// function something() {
+//     for (var i = 0; i < array.length; i++) {
+//         newArr.push(array[i]);
+//         array[i].onclick = function() {
+//             console.log(array[i].innerHTML);
+//         }
+//     }
+// }
+// something();
+document.querySelector("div").onclick = function() {
+    console.log("s");
+}
+for (var i = 0; i < array.length; i++) {
+    array[i].classList.add("elem-" + (i + 1));
+}
 
-button.addEventListener("click", displayDate);
+// array[0].onclick = function() {
+//     console.log("ass");
+// }
+
+// button.addEventListener("click", something);
